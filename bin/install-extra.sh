adeff6658bccbf9b0ba4a4e40b89084e44b5c748
GabrielCC <gabriel.croitoru.cristian@gmail.com>
2013-09-23 19:51:47 +0300

    added install git command

--- bin/install-extra.sh
+++ bin/install-extra.sh
@@ -2,4 +2,4 @@
 apt-get -y install debconf-utils
 echo mysql-server mysql-server/root_password password qwaszx | sudo debconf-set-selections
 echo mysql-server mysql-server/root_password_again password qwaszx | sudo debconf-set-selections
-apt-get -y install libmysqlclient-dev mysql-server libmagickcore-dev libmagickwand-dev
+apt-get -y install libmysqlclient-dev mysql-server libmagickcore-dev libmagickwand-dev git
